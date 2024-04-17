5e571c86d56d2265a740f630083d7c2ace238927
Lica Andrei <ovidiu.lica@student.upt.ro>
2024-04-18 01:14:48 +0300

    ui prettier part 1

--- hackathonApp/lib/Screens/RegisterScreen.dart
+++ hackathonApp/lib/Screens/RegisterScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'HomeScreen.dart';
@@ -7,100 +8,96 @@ class LogInScreen extends StatefulWidget {
 
   final String title;
 
-
   @override
   State<LogInScreen> createState() => _LogInScreenState();
 }
 
 class _LogInScreenState extends State<LogInScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final String username="partisipant";
-    final String password="parola01";
-    final Size screenSize=MediaQuery.of(context).size;
-    final TextEditingController usernameController=TextEditingController();
-    final TextEditingController passwordController=TextEditingController();
+    final String username = "partisipant";
+    final String password = "parola01";
+    final Size screenSize = MediaQuery.of(context).size;
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    void tryLogin([String text = ""]) {
+      if (usernameController.text == username &&
+          passwordController.text == password) {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const HomeScreen(
+            title: '',
+          ),
+        ));
+      } else {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: new Text("lolo"),
+                content: new Text("parola not ok"),
+              );
+            });
+      }
+    }
+
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
+      backgroundColor: const Color.fromRGBO(180, 57, 229, 34),
       body: Center(
-          child: Container(
-              padding: const EdgeInsets.all(20.0),
-              width: screenSize.width,
-              height: screenSize.height * 0.3,
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "HackHelp",
+            style: TextStyle(color: Colors.white, fontSize: 70),
+          ),
+          Container(
+
+              height: 200,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
               decoration: BoxDecoration(
-                color: const Color.fromARGB(34,135, 57, 229),
-                borderRadius: BorderRadius.circular(20.0),
+                color: const Color.fromRGBO(180, 106, 75, 100).withOpacity(0.1),
               ),
               child: Center(
-
                 child: Column(
-
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     TextFormField(
                       controller: usernameController,
+                      style: TextStyle(color: Colors.white),
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
+                        floatingLabelStyle: TextStyle(color: Colors.white),
+                        labelStyle: TextStyle(color: Colors.white),
                         labelText: 'Enter your username',
                       ),
                     ),
                     TextFormField(
                       controller: passwordController,
+                      style: TextStyle(color: Colors.white),
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: 'Enter your password',
+                        floatingLabelStyle: TextStyle(color: Colors.white),
+                        labelStyle: TextStyle(color: Colors.white),
+                        labelText: 'Enter Password',
                       ),
-                      onFieldSubmitted: (text){
-                        if(usernameController.text==username && passwordController.text == password){
-                          // showDialog(context: context, builder:
-                          // (BuildContext context){
-                          //   return AlertDialog(
-                          //     title: new Text("lolo"),
-                          //     content: new Text("parola ok"),
-                          //   );
-                          // }
-                          // );
-                          Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => const HomeScreen(title: '',),
-                              )
-                          );
-                        }
-                        else{
-                          showDialog(context: context, builder:
-                              (BuildContext context){
-                            return AlertDialog(
-                              title: new Text("lolo"),
-                              content: new Text("parola not ok"),
-                            );
-                          }
-                          );
-                        }
-                    },
+                      onFieldSubmitted: tryLogin,
                       obscureText: true,
                     ),
                   ],
                 ),
-              ))),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              )),
+          ElevatedButton(
+            onPressed: tryLogin,
+            child: Text("LogIn", style: TextStyle(color: Colors.white)),
+            style: ButtonStyle(
+                padding: MaterialStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
+                backgroundColor: MaterialStatePropertyAll<Color>(
+                    Color.fromARGB(255, 255, 131, 89))),
+          )
+        ],
+      )),
     );
   }
 }
